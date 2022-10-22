5c7327a3a3de8e7b7c3a6cc7dfa1d8f19e83177d
peng <1533431376@qq.com>
2022-10-22 12:48:19 +0800

    hexo 20221022

--- blog/js/51la.js
+++ blog/js/51la.js
@@ -1,4 +1,35 @@
-// fetch('https://v6-widget.51.la/v6/Jp8wwGQpp21utaFQ/quote.js').then(res => res.text()).then((data) => {
+
+
+// fetch("https://v6-widget.51.la/v6/JqodjwdqAdwbnVcF/quote.js")
+// .then(res => res.text())
+// .then(data => {
+//     let title = ["最近活跃", "今日人数", "今日访问", "昨日人数", "昨日访问", "本月访问", "总访问量"];
+//     // let num = data.match(/(?<=<\/span><span>).*?(?=<\/span><\/p>)/g)
+//     let num = data.match(/(<\/span><span>).*?(\/span><\/p>)/g);
+
+//     num = num.map(el => {
+//     let val = el.replace(/(<\/span><span>)/g, "");
+//     let str = val.replace(/(<\/span><\/p>)/g, "");
+//     return str;
+//     });
+
+//     let s = document.getElementById("statistic");
+
+//     // 自定义不显示哪个或者显示哪个，如下为不显示 最近活跃访客 和 总访问量
+//     let statistic = [];
+//     for (let i = 0; i < num.length; i++) {
+//     if (i == 0 || i == num.length - 1) continue;
+//     s.innerHTML += "<div><span>" + title[i] + "</span><span id=" + title[i] + ">" + num[i] + "</span></div>";
+//     //   queueMicrotask(() => {
+//     //     statistic.push(new CountUp(title[i], 0, num[i], 0, 2, CountUpOptions));
+//     //   });
+//     }
+// });
+
+
+
+
+// fetch('https://v6-widget.51.la/v6/JqodjwdqAdwbnVcF/quote.js').then(res => res.text()).then((data) => {
 //     let title = ['最近活跃', '今日人数', '今日访问', '昨日人数', '昨日访问', '本月访问', '总访问量']
 //     // let num = data.match(/(?<=<\/span><span>).*?(?=<\/span><\/p>)/g)
 //     let num = data.match(/(<\/span><span>).*?(\/span><\/p>)/g)
@@ -27,23 +58,23 @@
 //         if (i == 0 || i == num.length - 1) continue;
 //         s.innerHTML += '<div><span>' + title[i] + '</span><span id='+ title[i] + '>' + num[i] + '</span></div>'
 //         queueMicrotask(()=> {
-//           statistic.push(new CountUp(title[i], 0, num[i], 0, 2, CountUpOptions))
+//         //   statistic.push(new CountUp(title[i], 0, num[i], 0, 2, CountUpOptions))
 //         })
 //     }
     
-//     setTimeout(()=> {
-//       const throttleStatisticUP = btf.throttle(statisticUP, 200)
-//       function statisticUP () {
-//         let statisticElment = document.querySelector('.about-statistic.author-content-item');
-//         if(isInViewPortOfOne(statisticElment)) {
-//           for (let i = 0; i < num.length; i++) {
-//             if (i == 0 || i == num.length - 1) continue;
-//             statistic[i-1].start();
-//           }
-//           document.removeEventListener('scroll', throttleStatisticUP);
-//         }
-//       }
-//       document.addEventListener('scroll', throttleStatisticUP)
-//     })
+//     // setTimeout(()=> {
+//     //   const throttleStatisticUP = btf.throttle(statisticUP, 200)
+//     //   function statisticUP () {
+//     //     let statisticElment = document.querySelector('.about-statistic.author-content-item');
+//     //     if(isInViewPortOfOne(statisticElment)) {
+//     //       for (let i = 0; i < num.length; i++) {
+//     //         if (i == 0 || i == num.length - 1) continue;
+//     //         statistic[i-1].start();
+//     //       }
+//     //       document.removeEventListener('scroll', throttleStatisticUP);
+//     //     }
+//     //   }
+//     //   document.addEventListener('scroll', throttleStatisticUP)
+//     // })
     
 // });
