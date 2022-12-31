baaa438ecec3d1d626c5e917279bf76e797bcfe5
peng <1533431376@qq.com>
2022-12-31 18:02:45 +0800

    hexo 20221231

--- js/fps.js
+++ js/fps.js
@@ -1,1 +1,64 @@
-function fps(){var e=window.requestAnimationFrame||window.webkitRequestAnimationFrame||function(e){window.setTimeout(e,1e3/60)},n=0,t=Date.now(),o=Date.now(),i=function(){var a=Date.now(),d=a-o,u=Math.round(1e3/d);if(o=a,n++,a>1e3+t){u=Math.round(1e3*n/(a-t));document.getElementById("fps").innerHTML=`FPS:${u}`,n=0,t=a}e(i)};i()}document.addEventListener("pjax:complete",fps),document.addEventListener("DOMContentLoaded",fps);
+// document.addEventListener('pjax:complete', fps);
+// document.addEventListener('DOMContentLoaded', fps);
+// function fps(){
+// // if(window.localStorage.getItem("fpson")=="1"){ 
+// //如果要使博客设置上面的设置项能生效，就把上面一行取消注释
+// var rAF = function () {
+//     return (
+//         window.requestAnimationFrame ||
+//         window.webkitRequestAnimationFrame ||
+//         function (callback) {
+//             window.setTimeout(callback, 1000 / 60);
+//         }
+//     );
+// }();
+// var frame = 0;
+// var allFrameCount = 0;
+// var lastTime = Date.now();
+// var lastFameTime = Date.now();
+// var loop = function () {
+//     var now = Date.now();
+//     var fs = (now - lastFameTime);
+//     var fps = Math.round(1000 / fs);
+ 
+//     lastFameTime = now;
+//     // 不置 0，在动画的开头及结尾记录此值的差值算出 FPS
+//     allFrameCount++;
+//     frame++;
+ 
+//     if (now > 1000 + lastTime) {
+//         var fps = Math.round((frame * 1000) / (now - lastTime));
+//         // if(fps<=6){
+//         //     var kd=`<span style="color:#bd0000">卡成ppt</span>`
+//         // }
+//         // else if(fps<=10){
+//         //     var kd=`<span style="color:red">电竞级帧率</span>`
+//         // }
+//         // else if(fps<=14){
+//         //     var kd=`<span style="color:yellow">难受</span>`
+//         // }
+//         // else if(fps<24){
+//         //     var kd=`<span style="color:orange">卡</span>`
+//         // }
+//         // else if(fps<=40){
+//         //     var kd=`<span style="color:green">...</span>`
+//         // }
+//         // else{
+//         //     var kd=`<span style="color:#425aef">正常</span>`
+//         // }
+//         document.getElementById("fps").innerHTML=`FPS:${fps}`;
+//         // document.getElementById("fps").innerHTML=`FPS:${fps} ${kd}`;
+//         frame = 0;
+//         lastTime = now;
+//     };
+ 
+//     rAF(loop);
+// }
+
+// loop();
+// // }
+// // else{$("#fps").hide()}
+
+// //如果要使博客设置上面的设置项能生效，就把上面两行取消注释
+// }
+
